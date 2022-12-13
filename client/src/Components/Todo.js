dd4a16dbd0ccec7f31b23ed8e30d0d5ce312d3be
8rightrue <h1em0n1m@gmail.com>
2022-12-14 00:54:48 +0900

    솔로 프로젝트 클라이언트 및 서버 업로드
    솔로 프로젝트 클라이언트 및 서버 업로드

---
+++ client/src/Components/Todo.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Icon } from "@iconify/react";
+
+const Todo = ({ name, done, onToggle, onTrash, onRename }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [newName, setnewName] = useState(name);
+  return (
+    <div className="todo_frame">
+      <Icon
+        className={"checkbox" + (done ? "checked" : "unchecked")}
+        icon={"ph:cat" + (done ? "-fill" : "")}
+        onClick={() => onToggle(!done)}
+      />
+      {editMode ? (
+        <form
+          className="edit_form"
+          onSubmit={(ev) => {
+            ev.preventDefault();
+            onRename(newName);
+            name = newName;
+            setEditMode(false);
+          }}
+          onClick={() => setEditMode(false)}
+        >
+          <input
+            className="edit_input"
+            type="text"
+            value={name}
+            onChange={(ev) => setnewName(ev.target.value)}
+          />
+        </form>
+      ) : (
+        <div
+          className={`${done ? "checked" : "unchecked"}_task`}
+          onClick={() => setEditMode(true)}
+        >
+          {name}
+        </div>
+      )}
+      <Icon icon="bi:trash" className="trash" onClick={onTrash} />
+    </div>
+  );
+};
+
+export default Todo;
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+const tasks = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/todos", (req, res) => {
+  console.log("api/todos called!");
+  res.json(tasks);
+});
+
+app.post("/api/todo", (req, res) => {
+  console.log("Adding now!");
+  tasks.push(req.body);
+  res.json("todo added!");
+});
+
+app.delete("/api/delete/:id", (req, res) => {
+  const idx = req.params.id;
+  tasks.splice(idx, 1);
+  res.json("todo deleted!");
+});
+
+app.put("/api/update/name/:id", (req, res) => {
+  const idx = req.params.id;
+  tasks[idx].name = req.body.name;
+  res.json("todo updated!");
+});
+
+app.put("/api/update/check/:id", (req, res) => {
+  const idx = req.params.id;
+  tasks[idx].done = req.body.done;
+  res.json("Checking!");
+});
+
+app.listen(3070, () => {
+  console.log("Server listening on the port::3070");
+});
